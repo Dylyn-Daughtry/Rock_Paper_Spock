5a99e5891bce2fb512eb551b58413eeaf2458857
Dylyn-Daughtry <98605975+Dylyn-Daughtry@users.noreply.github.com>
2022-02-07 16:21:53 -0500

    Classes Created along with Display message tested

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Rock_Paper_Spock
 rock_paper_scissors_lizard_spock
+
+Rules
+Rock crushes Scissors   
+Scissors cuts Paper  
+Paper covers Rock  
+Rock crushes Lizard  
+Lizard poisons Spock  
+Spock smashes Scissors  
+Scissors decapitates Lizard  
+Lizard eats Paper  
+Paper disproves Spock  
+Spock vaporizes Rock  
+
---
+++ game.py
@@ -0,0 +1,27 @@
+
+
+class Game:
+
+    
+
+    def run_game(self):
+        self.display_welcome()
+
+
+    def display_welcome(self):
+        print(f'Welcome Player(s) to Rock, Paper, Scissors, Lizard, Spock \n' + 
+        'Rock crushes Scissors\n' +
+        'Scissors cuts Paper\n' +
+        'Paper covers Rock\n' +
+        'Rock crushes Lizard\n' +
+        'Lizard poisons Spock\n' +
+        'Spock smashes Scissors\n' + 
+        'Scissors decapitates Lizard\n' + 
+        'Lizard eats Paper \n' +
+        'Paper disproves Spock \n' + 
+        'Spock vaporizes Rock \n') 
+
+
+    
+    
+     
